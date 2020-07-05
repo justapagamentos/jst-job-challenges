44a64cbdb29309d6b652c434725a0c2f60400c12
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-05 02:17:17 -0300

    feature-sass-e-linters: ajuste nos arquivos padrão

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
-import App from "./App";
+import App from "./components/App/App.jsx";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
