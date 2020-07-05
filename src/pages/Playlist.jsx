e563f64ec37901b212f90d16e129f8542f3a20d4
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-05 19:57:15 -0300

    feature-estrutura: adição do router e das páginas

---
+++ src/pages/Playlist.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Playlist extends Component {
+  render() {
+    return (
+      <div className="Playlist">
+        <h1>Playlist</h1>
+      </div>
+    );
+  }
+}
+
+export default Playlist;
