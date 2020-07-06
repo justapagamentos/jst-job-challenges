fa3cd565d814ea011dd897ad8d586cb2ad1d2fab
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-06 14:41:15 -0300

    feature-home: adição da estrtutura da seção github

--- src/components/GithubBox/GithubBox.jsx
+++ src/components/GithubBox/GithubBox.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 
+import github from "../../assets/github.svg";
 import "./GithubBox.scss";
 
 class GithubBox extends Component {
   render() {
     return (
       <div className="GithubBox">
-        <h1>GithubBox</h1>
+        <div className="GithubContainer">
+          <h3>Colabore com nosso projeto!</h3>
+          <div className="gitLink">
+            <div className="gitIcon">
+              <img src={github} alt="github-icon" />
+            </div>
+            <a href="https://github.com/dxxglas/jst-job-challenges">
+              /dxxglas/jst-job-challenges
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
