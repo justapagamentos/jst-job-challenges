15478dcb45d564fb1dca09fa97ae37c81b1fe80a
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-06 10:50:16 -0300

    feature-home: adição da estrutura da search-bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
 
+import search from "../../assets/search.svg";
 import "./SearchBar.scss";
 
 class SearchBar extends Component {
   render() {
     return (
-      <div className="SearchBar">
-        <h1>SearchBar</h1>
-      </div>
+      <form className="SearchBar">
+        <div className="sBox">
+          <input type="search" placeholder="Cole o link da playlist aqui..." />
+          <button>
+            <img src={search} alt="search-icon" />
+          </button>
+        </div>
+      </form>
     );
   }
 }
