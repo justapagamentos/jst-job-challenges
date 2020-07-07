5801b8e874554bed73f8b659fdcb5d562ee37d33
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-06 22:43:32 -0300

    feature-playlist: mudança no nome do componente tablecard

---
+++ src/components/TableCard/TableCard.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "./TableCard.scss";
+
+class TableCard extends Component {
+  render() {
+    return (
+      <div className="TableCard">
+        <h1>TableCard</h1>
+      </div>
+    );
+  }
+}
+
+export default TableCard;
